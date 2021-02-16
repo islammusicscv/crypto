c974966e82a4d0175eb0f7e1df001c2faf2228e9
islammusicscv <islam.music@scv.si>
2021-02-16 19:33:05 +0100

    login pripraljven - a nedokoncan

--- register.php
+++ register.php
@@ -12,7 +12,7 @@
             <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
             <div class="divider-custom-line"></div>
         </div>
-        <!-- Contact Section Form-->
+        <!-- Registration Section Form-->
         <div class="row">
             <div class="col-lg-8 mx-auto">
                 <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
@@ -53,8 +53,9 @@
                         </div>
                     </div>
                     <br />
-                            <div id="success"></div>
-                            <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Pošlji</button></div>
+                    <div id="success"></div>
+                    <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton"
+                            type="submit">Pošlji</button></div>
                 </form>
             </div>
         </div>
