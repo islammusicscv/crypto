762c0178f895be99790e1dc78b264040c46f2870
islammusicscv <islam.music@scv.si>
2021-02-18 18:28:29 +0100

    login logika

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-include_once "header.php";           
+include_once "header.php";   
+
+if (isset($_SESSION['user_id'])) {
+    echo 'PRIJAVLJEN';
+}
+else {
+    echo 'NEPRIJAVLJEN';
+}
+
 ?>
 
 <a href="users.php?username=vssscv&visina=190&teza=90&oci=modre">Uporabniki</a>
